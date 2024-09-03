a9641adc8900793925fca1147ab83950add53929
Marek <marek@MacBook-Air-Marek.local>
2024-09-03 12:05:16 +0200

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import FacebookPixel from "./components/facebookPixel";
+import Hotjar from "./components/hotjar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Hotjar />
       <FacebookPixel />
       <body className={inter.className}>{children}</body>
     </html>
